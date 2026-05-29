7718dccfb22aa9a5562601ccfa50f6cb0e486004
Mohammad Faraz Rajput <farazrajput112@gmail.com>
2026-05-30 01:30:45 +0530

    fix: logging issue

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,8 +3,19 @@ import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+logger = logging.getLogger("DataIngestion")
+os.makedirs("logs", exist_ok=True)
+file_handler = logging.FileHandler(os.path.join("logs", "ingestion.logs"))
+stream_handler = logging.StreamHandler()
+detailed_log = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+file_handler.setFormatter(detailed_log)
+stream_handler.setFormatter(detailed_log)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+logger.info("Starting the ingestion part.")
 
 class DataIngestion():
     def __init__(self, db_name: str, collection_name:str):
--- test_ingestion.py
+++ test_ingestion.py
@@ -1,6 +1,6 @@
 from src.data_ingestion import DataIngestion
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 DI = DataIngestion(db_name = "sample_mflix", collection_name="movies")
 DI.connect()
